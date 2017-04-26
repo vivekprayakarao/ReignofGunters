95c07e0a4835dc9f5cc45c48c40dd5bba2b7ac01
vivek <vivek.iitj@gmail.com>
2017-04-26 14:06:53 -0400

    removed build and devel

--- build/project6/adventure_gripper/adventure_arm_bringup/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vivek/project6_ws/src/project6/adventure_gripper/adventure_arm_bringup
-# Build directory: /home/vivek/project6_ws/build/project6/adventure_gripper/adventure_arm_bringup
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/pvivek_project6/adventure_simulator/adventure_gazebo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vivek/project6_ws/src/pvivek_project6/adventure_simulator/adventure_gazebo
-# Build directory: /home/vivek/project6_ws/build/pvivek_project6/adventure_simulator/adventure_gazebo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
