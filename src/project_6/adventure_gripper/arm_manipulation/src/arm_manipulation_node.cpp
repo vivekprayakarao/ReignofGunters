1cfe6d50c04a89cdc28bf5c3fd5e9579db68a320
vivek <vivek.iitj@gmail.com>
2017-05-10 18:11:54 -0400

    modified arm code

--- src/project_6/adventure_gripper/arm_manipulation/src/arm_manipulation_node.cpp
+++ src/project_6/adventure_gripper/arm_manipulation/src/arm_manipulation_node.cpp
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
   ros::NodeHandle node_handle;  
   ros::AsyncSpinner spinner(1);
   spinner.start();
-  sleep(20.0);
+  sleep(2.0);
   
 
   moveit::planning_interface::MoveGroup group("arm");
@@ -33,14 +33,23 @@ int main(int argc, char **argv)
   std::cout<<group.getCurrentPose();
 
 
+  /*geometry_msgs::Pose target_pose1;
+  target_pose1.orientation.x =  -0.993009;
+  target_pose1.orientation.y = 0.0457311;
+  target_pose1.orientation.z = 0.108702;
+  target_pose1.orientation.w = 0.005008;
+  target_pose1.position.x = 0.259775;
+  target_pose1.position.y = -0.0184402;
+  target_pose1.position.z =  0.256358;*/
+
   geometry_msgs::Pose target_pose1;
-  target_pose1.orientation.x = 0.302278;
-  target_pose1.orientation.y = -0.639938;
-  target_pose1.orientation.z = 0.301736;
-  target_pose1.orientation.w = 0.638798;
-  target_pose1.position.x = 0.0351457;
-  target_pose1.position.y = -0.0302251;
-  target_pose1.position.z = 0.442074;
+  target_pose1.orientation.x =  0.922849;
+  target_pose1.orientation.y = 0.00235894;
+  target_pose1.orientation.z = -0.385154;
+  target_pose1.orientation.w = 0.00098235;
+  target_pose1.position.x = 0.166023;
+  target_pose1.position.y = 0.000541269;
+  target_pose1.position.z =  0.273873;
   group.setPoseTarget(target_pose1);
 
   moveit::planning_interface::MoveGroup::Plan my_plan;
@@ -48,7 +57,7 @@ int main(int argc, char **argv)
 
   ROS_INFO("Visualizing plan 1 (pose goal) %s",success?"":"FAILED");    
 
-  sleep(5.0);
+  //sleep(5.0);
  
   if (1)
   {
@@ -63,7 +72,49 @@ int main(int argc, char **argv)
  
   /* Uncomment below line when working with a real robot*/
   group.move();
+  sleep(2.0);
+
+  geometry_msgs::Pose target_pose2;
+  target_pose2.orientation.x =  0.880169;
+  target_pose2.orientation.y = 0.136085;
+  target_pose2.orientation.z = -0.449396;
+  target_pose2.orientation.w = 0.0694798;
+  target_pose2.position.x = 0.29454;
+  target_pose2.position.y = 0.0743012;
+  target_pose2.position.z =  0.147096;
+  group.setPoseTarget(target_pose2);
   
+  // Moving to a pose goal
+ 
+  /* Uncomment below line when working with a real robot*/
+  group.move();
+
+  sleep(2.0);
+
+  geometry_msgs::Pose target_pose3;
+  target_pose3.orientation.x =  -0.844858;
+  target_pose3.orientation.y = 0.237063;
+  target_pose3.orientation.z = 0.461766;
+  target_pose3.orientation.w = 0.129571;
+  target_pose3.position.x = 0.283182;
+  target_pose3.position.y = -0.135952;
+  target_pose3.position.z =  0.103461;
+  group.setPoseTarget(target_pose3);
+
+  group.move();
+
+  sleep(2.0);
+  geometry_msgs::Pose target_pose4;
+  target_pose4.orientation.x =  0.922849;
+  target_pose4.orientation.y = 0.00235894;
+  target_pose4.orientation.z = -0.385154;
+  target_pose4.orientation.w = 0.00098235;
+  target_pose4.position.x = 0.166023;
+  target_pose4.position.y = 0.000541269;
+  target_pose4.position.z =  0.273873;
+  group.setPoseTarget(target_pose4);
+
+  group.move();
   ros::shutdown();  
   return 0;
 }
